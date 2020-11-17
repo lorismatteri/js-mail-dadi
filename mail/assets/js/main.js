ba51c7c76090006beb7f43de4e5cecead257b5be
Loris Matteri <loris.matteri93@gmail.com>
2020-11-17 16:43:11 +0100

    Mail
    Completato esercizio lista mail, ho provato a usare for ma non mi funzionava così ho usato solo if con operatore logico ||

---
+++ mail/assets/js/main.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente la sua email,
+var userMail = prompt('Inserisci il tuo indirizzo e-mail');
+
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+var mails = ['cristianoronaldo@gmail.com', 'diozlatan@gmail.com', 'leomessi@gmail.com'];
+
+if (mails[0] == userMail || mails[1] == userMail || mails[2] == userMail) {
+  document.getElementById('welcome').innerHTML = "Ciao " + userMail + ", benvenuto nel lato oscuro della Forza!";
+} else {
+  document.getElementById('welcome').innerHTML = "Addio " + userMail + ", non sei degno!";
+}
